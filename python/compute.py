4bae7ba624e87de119600355463676a03fc57715
Xinhe Feng <xf459@nyu.edu>
2018-04-27 23:15:59 -0400

    seperate compute func

---
+++ python/compute.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 27 23:14:49 2018
+
+@author: firebug
+"""
+
+def computeVpScore(filePath):
+    
+    #Mat image_origin = imread(filePath);
+    #Mat img_gray, img_float;
+    #cvtColor(image_origin, img_gray, CV_RGB2GRAY);
+    #img_gray.convertTo(img_float, CV_32F);
+    image_origin=cv2.imread(filePath)
+    img_gray=cv2.cvtColor(image_origin, cv2.COLOR_BGR2GRAY)
+    img_float=np.float(img_gray)
+    
+    ow,oh=image_origin.shape
+    n_theta = 36;
+    width = 128;
+    scale_factor = width/oh;
+     
+    gw,gh=img_gray.shape
+                                        
+	#Mat image(img_gray.rows * scale_factor, width, CV_32F);
+    image=np.zeros((gw*scale_factor,width), np.float32)
+	#resize(img_float, image, image.size());
+    image=cv2.resize(img_float,image.shape,interpolation=cv2.INTER_AREA)
+     #int m = image.rows, n = image.cols;
+    m,n=image.shape
+ 
+     #float scores[85][128];
+    scores=np.zeros((m,n),np.float32)
+	#float ***gabors = new float**[m];
+    gabors = np.zeros((m,n,n_theta),np.float32)
+	#for_each(gabors, gabors + m, [n](float** &x) {x = new float*[n]; });
+	#for_each(gabors, gabors + m, [n, n_theta](float** x) {for_each(x, x + n, [&, n_theta](float* &y) { y = new float[n_theta]; }); });
+
+#Mat oddfiltered(image.rows, image.cols, CV_32F), evenfiltered(image.rows, image.cols, CV_32F);
+    # oddfiltered=np.zeros((m,n),np.float32)
+    # evenfiltered=np.zeros((m,n),np.float32)
+   
+    oddfiltered=cv2.filter2D(image,-1,oddKernels)
+    evenfiltered=cv2.filter2D(image,-1,evenKernels)
+   
+    for t in range(n_theta):
+        for i in range (m):
+            for j in range(n):
+                gabors[i][j][t]=np.power(oddfiltered[i][j],2.0)+np.power(evenfiltered[i][j],2.0)
+     
+    directions=np.zeros((m,n),np.uint8)
+    for i in range(m):
+        for j in range(n):
+            directions[i][j]=np.max(gabors[i][j])-gabors[i][j][0]
+    
+    thresh=2.0*180/n_theta
+    #r=(m+n)/7
+    #r_dia=np.sqrt(m*m+n*n)
+    gamma=0
+    c=0
+    for i in range(m):
+        for j in range(n):
+            tempScore=0
+            for i1 in range(i+1,m):
+                for j1 in range(n):
+                    c=directions[i1][j1]/n_theta*180.0
+                    gamma=arcMatrix[i1-1][j1-j+200]["arc"]
+                    if(np.abs(c-gamma)<thresh):
+                        tempScore +=1
+            scores[i][j]=tempScore    
+    
+    score_min,score_max,p_min,p_max=cv2.minMaxLoc(scores)
+   # scale=score_max/255.0
+    x=p_max.x/scale_factor
+    y=p_max.y/scale_factor
+    # 	cv::circle(image_origin, cvPoint(p_max.x / scale_factor, p_max.y / scale_factor), 10, Scalar(0), 5, 8, 0);
+    cv2.circle(image_origin,(x,y),10,(0,0,0),5,8,0)
+    return image_origin     
